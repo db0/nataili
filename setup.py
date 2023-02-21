a4eff2f72c229495b1222951c7660575b86f01d2
hlky <106811348+hlky@users.noreply.github.com>
2023-02-21 21:36:45 +0000

    v0.2.69

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ os.environ["PIP_EXTRA_INDEX_URL"] = "https://download.pytorch.org/whl/cu117"
 
 setup(
     name="nataili",
-    version="0.2.50",
+    version="0.2.69",
     description="",
     packages=find_packages(),
     install_requires=[
