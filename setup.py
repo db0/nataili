e8b3275577cb999e6e05d6b18e62d1e32bd65bf2
hlky <106811348+hlky@users.noreply.github.com>
2023-02-24 10:08:47 +0000

    0.2.9004

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as reqstxt:
 
 setup(
     name="nataili",
-    version="0.2.9003",
+    version="0.2.9004",
     description="",
     packages=find_packages(),
     install_requires=requirements,
